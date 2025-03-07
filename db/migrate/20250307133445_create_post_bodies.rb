c1f668a016e6633198a62c989f790f02c03fe5d6
KoheiYagi <192582422+koheiyagi1206@users.noreply.github.com>
2025-03-07 13:54:06 +0000

    [Fix]fixed foreign_key for posts and post_bodies.

--- db/migrate/20250307133445_create_post_bodies.rb
+++ db/migrate/20250307133445_create_post_bodies.rb
@@ -1,10 +1,10 @@
 class CreatePostBodies < ActiveRecord::Migration[6.1]
   def change
     create_table :post_bodies do |t|
-      t.integer   :post_id,     null: false, foreign_key: true
-      t.text      :text,        default: ""
-      t.string    :image_title, default: ""
-      t.binary    :image_body,  default: ""
+      t.references  :post,        null: false, foreign_key: true
+      t.text        :text,        default: ""
+      t.string      :image_title, default: ""
+      t.binary      :image_body,  default: ""
 
       t.timestamps
     end
