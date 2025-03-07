1402ad2eabea14854aabcd79f251bd16db87a6c8
KoheiYagi <192582422+koheiyagi1206@users.noreply.github.com>
2025-03-07 13:21:35 +0000

    [Add]added post_bodies_controller.

---
+++ app/controllers/standard/post_bodies_controller.rb
@@ -0,0 +1,12 @@
+class Standard::PostBodiesController < ApplicationController
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
---
+++ app/helpers/standard/post_bodies_helper.rb
@@ -0,0 +1,2 @@
+module Standard::PostBodiesHelper
+end
