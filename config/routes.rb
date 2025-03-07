60d10db82de634c122c4a7955ab0a806983aa6ea
KoheiYagi <192582422+koheiyagi1206@users.noreply.github.com>
2025-03-07 14:04:29 +0000

    [Update]updated routes for post_bodies.

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,10 @@ Rails.application.routes.draw do
     registrations:  "standard/registrations",
     sessions:       "standard/sessions"
   }
-  resources :users,       controller: "standard/users",       only:[:show, :edit, :update, :destroy]
-  resources :posts,       controller: "standard/posts"
-  resources :post_bodies, controller: "standard/post_bodies", only:[:create, :update, :destroy]
+  resources :users, controller: "standard/users", only:[:show, :edit, :update, :destroy]
+  resources :posts, controller: "standard/posts" do
+    resources :post_bodies, controller: "standard/post_bodies", only:[:create, :update, :destroy]
+  end
   get "/mypage", to: "standard/users#mypage"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
