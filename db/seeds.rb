e412f16506b899d4da4444a83e39a9a1ecc2ffea
KoheiYagi <192582422+koheiyagi1206@users.noreply.github.com>
2025-03-07 16:02:08 +0000

    [Fix]fixed belongs_to for post_body.

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,19 @@ User.create!(
   email:      "user_1@mail.com",
   password:   "123456"
 )
+
+# デバッグ値：user_1投稿
+Post.create!(
+  user_id:  1,
+  title:    "game no stage1-1. "
+)
+
+PostBody.create!(
+  post_id:  1,
+  text:     "this stage is very easy!"
+)
+
+PostBody.create!(
+  post_id:  1,
+  text:     "this character is very strong!"
+)
