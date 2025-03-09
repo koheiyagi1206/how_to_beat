78f84adbc2b8b0747c337fc201b601ff89aa1016
KoheiYagi <192582422+koheiyagi1206@users.noreply.github.com>
2025-03-09 06:23:27 +0000

    [Add]added controllers for comments and tags.

---
+++ app/controllers/standard/comments_controller.rb
@@ -0,0 +1,2 @@
+class Standard::CommentsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,10 @@ Rails.application.routes.draw do
   resources :users, controller: "standard/users", only:[:show, :edit, :update, :destroy]
   resources :posts, controller: "standard/posts" do
     resources :post_bodies, controller: "standard/post_bodies", only:[:create, :update, :destroy]
+    resources :comments,    controller: "standard/comments",    only:[:create, :update, :destroy]
   end
-  resources :groups, controller: "standard/groups"
+  resources :groups,  controller: "standard/groups"
+  resources :tags,    controller: "standard/tags", only:[:index, :update, :destroy]
   get "/mypage", to: "standard/users#mypage"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
