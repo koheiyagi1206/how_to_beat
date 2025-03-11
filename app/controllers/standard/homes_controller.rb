64bc689aa144a0ef6c79c2433dbf562682942e39
KoheiYagi <192582422+koheiyagi1206@users.noreply.github.com>
2025-03-11 06:45:49 +0000

    [Update]updated layout and top page image include.

--- app/controllers/standard/homes_controller.rb
+++ app/controllers/standard/homes_controller.rb
@@ -1,7 +1,7 @@
 class Standard::HomesController < ApplicationController
 
   def top
-    @new_arrivals_posts   = Post.order(updated_at: :desc)
+    @new_arrivals_posts   = Post.order(updated_at: :desc).includes(:post_image_blob)
     @new_arrivals_comments = Comment.order(updated_at: :desc).includes(:user, :post)
   end
 
