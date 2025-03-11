f860b65d400f844da4a42a7b1da456f8c1977110
KoheiYagi <192582422+koheiyagi1206@users.noreply.github.com>
2025-03-11 13:41:42 +0000

    [Update]updated top page.

--- app/controllers/standard/homes_controller.rb
+++ app/controllers/standard/homes_controller.rb
@@ -1,8 +1,8 @@
 class Standard::HomesController < ApplicationController
 
   def top
-    @new_arrivals_posts   = Post.order(updated_at: :desc).includes(:post_image_blob)
-    @new_arrivals_comments = Comment.order(updated_at: :desc).includes(:user, :post)
+    @new_arrivals_posts     = Post.order(updated_at: :desc).includes(:post_image_blob)
+    @new_arrivals_comments  = Comment.order(updated_at: :desc).includes(:user, :post)
   end
 
   def about
