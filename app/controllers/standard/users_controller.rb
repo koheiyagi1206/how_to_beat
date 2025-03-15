f58baf6b21a53af447c4dbc4a7675dbe636db4fe
KoheiYagi <192582422+koheiyagi1206@users.noreply.github.com>
2025-03-15 13:12:39 +0000

    [Update]updated MyPage view and installed kaminari.

--- app/controllers/standard/users_controller.rb
+++ app/controllers/standard/users_controller.rb
@@ -1,6 +1,9 @@
 class Standard::UsersController < ApplicationController
+  before_action :comment_many_order_sort
 
   def mypage
+    @me       = User.find(current_user.id)
+    @my_posts = Post.where(user_id: @me.id).order(updated_at: :desc).page(params[:page]).per(6)
   end
 
   def show
