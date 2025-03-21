6edad20fb1d3d62533f024612bb8aa18c7edc1bd
KoheiYagi <192582422+koheiyagi1206@users.noreply.github.com>
2025-03-21 17:32:55 +0000

    [Update]updated User CRUD.

--- app/controllers/standard/users_controller.rb
+++ app/controllers/standard/users_controller.rb
@@ -7,16 +7,35 @@ class Standard::UsersController < ApplicationController
   end
 
   def show
+    @target_user  = User.find(params[:id])
+    @target_posts = Post.where(user_id: @target_user.id).order(updated_at: :desc).page(params[:page]).per(6)
   end
 
   def edit
+    @target_user = User.find(params[:id])
   end
 
   def update
+    @target_user = User.find(params[:id])
+
+    if @target_user.update(user_params)
+      redirect_to user_path(@target_user)
+    else
+      flash[:notice] = "Please Enter the User Name and E-Mail."
+      render :edit
+    end
+
   end
 
   def destroy
+    target_user = User.find(params[:id])
+    target_user.destroy
+    redirect_to root_path, notice: "Thank You for Use This Site!"
   end
 
+  private
+    def user_params
+      params.require(:user).permit(:user_name, :email)
+    end
 
 end
