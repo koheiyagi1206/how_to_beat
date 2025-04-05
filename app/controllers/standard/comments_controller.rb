c9d76dd1da11a68fd7b1af0b6fd61518f9404904
KoheiYagi <192582422+koheiyagi1206@users.noreply.github.com>
2025-04-05 08:26:49 +0000

    [Fix]fixed code layouts.

--- app/controllers/standard/comments_controller.rb
+++ app/controllers/standard/comments_controller.rb
@@ -18,7 +18,7 @@ class Standard::CommentsController < ApplicationController
 
 
   private
-  def comment_params
-    params.require(:comment).permit(:comment)
-  end
+    def comment_params
+      params.require(:comment).permit(:comment)
+    end
 end
